4aa5c4aa4e9ea59ad1be738914ac8e3cb762329c
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-06-15 16:49:53 +0300

    Added a component for the creation and editing of recipes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipePlaceholderComponent } from './recipes/recipe-placeholder/recipe-placeholder.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -11,7 +12,9 @@ const appRoutes: Routes = [
   {
     path: 'recipes', component: RecipesComponent, children: [
       { path: '', component: RecipePlaceholderComponent },
+      { path: 'new', component: RecipeEditComponent },
       { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent }
     ]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipePlaceholderComponent } from './recipes/recipe-placeholder/recipe-placeholder.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { RecipePlaceholderComponent } from './recipes/recipe-placeholder/recipe-
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipePlaceholderComponent
+    RecipePlaceholderComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
