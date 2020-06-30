27d7dc8c0c3435cbd7aa151547b8b749eb567ee4
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-06-30 17:21:39 +0300

    Modules
    - Starting to split the application into modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RecipesModule } from './recipes/recipes.module';
 import { PlaceholderDirective } from './../common/placeholder/placeholder.directive';
 import { AlertComponent } from '../common/alert/alert.component';
 import { AuthInterceptorService } from './services/auth.interceptor.service';
@@ -14,31 +15,20 @@ import { DropdownDirective } from './../common/dropdown.directive';
 import { HeaderComponent } from './header/header.component';
 
 import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipePlaceholderComponent } from './recipes/recipe-placeholder/recipe-placeholder.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+
 import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     LoadingSpinnerComponent,
     DropdownDirective,
     PlaceholderDirective,
-    RecipePlaceholderComponent,
-    RecipeEditComponent,
     AuthComponent,
     AlertComponent
   ],
@@ -47,7 +37,8 @@ import { AuthComponent } from './auth/auth.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecipesModule
   ],
   providers: [ShoppingListService, RecipeService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent],
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,35 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipePlaceholderComponent } from './recipe-placeholder/recipe-placeholder.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipePlaceholderComponent,
+    RecipeEditComponent,
+  ],
+  imports: [
+    RouterModule, CommonModule, ReactiveFormsModule
+  ],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipePlaceholderComponent,
+    RecipeEditComponent,
+  ]
+})
+export class RecipesModule {
+
+}
